61d922b69476921c3e3935da28af7d6cc46d7267
Phil Howard <phil@gadgetoid.com>
2020-12-08 19:40:21 +0000

    Add test coverage for relocs

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,4 +1,5 @@
 import argparse
+import pathlib
 
 import pytest
 
@@ -8,3 +9,9 @@ def parsers():
     parser = argparse.ArgumentParser()
     parser.add_argument('--debug', action='store_true', help='Enable exception traces')
     return parser, parser.add_subparsers(dest='command', help='Commands')
+
+
+@pytest.fixture
+def test_resources(request):
+    # Get path to "test_relocs" resource dir
+    return pathlib.Path(request.module.__file__).with_suffix('')
