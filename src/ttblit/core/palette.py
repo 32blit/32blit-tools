8e089226d081229e89fe80d628a853d354fd89f2
Alistair Buxton <a.j.buxton@gmail.com>
2021-01-23 00:17:06 +0000

    Clean up Palette code and add some tests
    Removes some unused code, simplifies tostruct(), and tests that
    get_entry() does the right thing with and without strict mode.
    
    Also adds an __iter__ method to Palette, which is not actually
    used, but is just added for completeness.

--- src/ttblit/core/palette.py
+++ src/ttblit/core/palette.py
@@ -18,13 +18,16 @@ class Colour():
     def __getitem__(self, index):
         return [self.r, self.g, self.b][index]
 
+    def __repr__(self):
+        return f'Colour{self.r, self.g, self.b}'
+
 
 class Palette():
     def __init__(self, palette_file=None):
         self.transparent = None
         self.entries = []
 
-        if type(palette_file) is Palette:
+        if isinstance(palette_file, Palette):
             self.transparent = palette_file.transparent
             self.entries = palette_file.entries
             return
@@ -143,28 +146,11 @@ class Palette():
         else:
             raise TypeError(f'Colour {r}, {g}, {b}, {a} does not exist in palette!')
 
-    def tolist(self):
-        result = []
-        for r, g, b, a in self.entries:
-            result.append(r)
-            result.append(g)
-            result.append(b)
-            result.append(a)
-        return result
-
-    def tobytes(self):
-        return bytes(self.tolist())
-
     def tostruct(self):
-        result = []
-        for r, g, b, a in self.entries:
-            result.append({
-                'r': r,
-                'g': g,
-                'b': b,
-                'a': a,
-            })
-        return result
+        return [dict(zip('rgba', c)) for c in self.entries]
+
+    def __iter__(self):
+        return iter(self.entries)
 
     def __len__(self):
         return len(self.entries)
@@ -177,7 +163,7 @@ def type_palette(palette_file):
     # Only used as a type in argparse.
     # This wrapper around Palette traps errors and
     # raises in a way that's visible to the user
-    if type(palette_file) is Palette:
+    if isinstance(palette_file, Palette):
         return palette_file
 
     try:
