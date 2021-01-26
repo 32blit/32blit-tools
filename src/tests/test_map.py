111ff4f05300f4021968810d5327305c194ea39a
Phil Howard <phil@gadgetoid.com>
2021-01-26 14:31:51 +0000

    Add test for sorted map layers

--- src/tests/test_map.py
+++ src/tests/test_map.py
@@ -49,6 +49,30 @@ def test_map_tiled_struct(subparser):
     assert output == struct.pack('<4sBHHH4B', b'MTMX', 0, 4, 1, 1, 0, 1, 2, 3)
 
 
+def test_map_tiled_layer_reorder(subparser):
+    from ttblit.asset.builders import map
+
+    map = map.MapAsset(subparser)
+
+    map.output_struct = True
+
+    output = map.tiled_to_binary('''<?xml version="1.0" encoding="UTF-8"?>
+<map version="1.2" tiledversion="1.3.2" orientation="orthogonal" renderorder="right-down" compressionlevel="-1" width="4" height="1" tilewidth="8" tileheight="8" infinite="0" nextlayerid="2" nextobjectid="1">
+ <layer id="2" name="Tile Layer 2" width="4" height="1">
+  <data encoding="csv">
+5,6,7,8
+</data>
+ </layer>
+ <layer id="1" name="Tile Layer 1" width="4" height="1">
+  <data encoding="csv">
+1,2,3,4
+</data>
+ </layer>
+</map>
+''')
+    assert output == struct.pack('<4sBHHH8B', b'MTMX', 0, 4, 1, 2, 0, 1, 2, 3, 4, 5, 6, 7)
+
+
 def test_map_empty_tiled_remap_empty(subparser):
     from ttblit.asset.builders import map
 
