c8cea9fc18b634d47d76ab3895706c677c7c54ff
Phil Howard <phil@gadgetoid.com>
2021-01-27 20:22:28 +0000

    Move test files into one resources dir

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -14,4 +14,4 @@ def parsers():
 @pytest.fixture
 def test_resources(request):
     # Get path to "test_relocs" resource dir
-    return pathlib.Path(request.module.__file__).with_suffix('')
+    return pathlib.Path(request.module.__file__).parent / 'resources'
