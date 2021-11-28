553a8d7531754550c3967838f0f8f4b95b4da1ac
Charlie Birks <charlie@daft.games>
2021-11-28 22:44:21 +0000

    Add the addresses in long calls from ITCM to flash to the reloc list
    This depends on the veneers being one 32-bit instruction followed by the address.

--- src/ttblit/tool/relocs.py
+++ src/ttblit/tool/relocs.py
@@ -86,6 +86,35 @@ def relocs_cli(bin_file, elf_file, output):
 
                     reloc_offsets.append(flash_offset)
 
+        # fixup ITCM veneers for long calls back to flash
+        itcm_section = elffile.get_section_by_name('.itcm')
+        if itcm_section:
+            itcm_section_data = itcm_section.data()
+
+            for i in range(symtable.num_symbols()):
+                sym = symtable.get_symbol(i)
+
+                if sym.name.endswith('_veneer') and sym['st_value'] < 0xF800:
+                    # find where the target function is (strip leading __ and trailing _veneer from name)
+                    orig_sym = symtable.get_symbol_by_name(sym.name[2:-7])[0]
+                    target_addr = orig_sym['st_value']
+
+                    # make sure it's in flash
+                    if target_addr < 0x90000000:
+                        continue
+
+                    # find the function in flash
+                    itcm_offset = (sym['st_value'] - itcm_text_start) & ~1 # clear thumb bit
+                    flash_offset = itcm_offset + itcm_data
+
+                    # check for the address where we expect it
+                    # (ldr.w pc, [pc] followed by the address)
+                    ex_addr, = struct.unpack('<I', itcm_section_data[itcm_offset + 4:itcm_offset + 8])
+                    
+                    assert ex_addr == target_addr
+
+                    reloc_offsets.append(flash_offset + 4)
+
         with open(output, 'wb') as out_f:
             all_offsets = got_offsets + init_offsets + fini_offsets + reloc_offsets
             all_offsets.sort()
